24e8eb07ba293323925984664dc27b52c92b38e8
rodrigo183 <digo.marqualves@gmail.com>
2022-12-13 12:32:35 -0300

    implementa api de teste

--- src/main/java/com/teste/veiculos/VeiculosApplication.java
+++ src/main/java/com/teste/veiculos/VeiculosApplication.java
@@ -2,10 +2,19 @@ package com.teste.veiculos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class VeiculosApplication {
 
+	@GetMapping
+	public String getHomeTeste() {
+		return "Veiculos - Home";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(VeiculosApplication.class, args);
 	}
